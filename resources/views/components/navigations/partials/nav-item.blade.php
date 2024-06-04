0d85e1f6d577cb153a1132546db052162e187f7a
sid <dev.mogyahidansid@gmail.com>
2024-06-04 17:22:37 +0800

    Implement sub-links on nav-item and sidebar

--- resources/views/components/navigations/partials/nav-item.blade.php
+++ resources/views/components/navigations/partials/nav-item.blade.php
@@ -2,11 +2,21 @@
     'url' => '',
     'title' => '',
     'isActive' => true,
+    'hasSubLinks' => false,
 ])
 
-<li class="rounded p-2.5 {{ $isActive ? 'bg-gray-200' : 'bg-transparent' }}">
+<li class="rounded p-2.5 {{ $isActive ? 'bg-gray-200' : 'bg-transparent hover:bg-gray-100 block' }}">
     <a href="{{ $url }}" class="flex items-center text-gray-900 text-sm">
-        {{ $icon }}
-        <span class="ml-2">{{ $title }}</span>
+        @isset($icon)
+            {{ $icon }}
+        @endisset
+
+        <div class="flex items-center justify-between w-full">
+            <span class="ml-2">{{ $title }}</span>
+
+            @if ($hasSubLinks)
+                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m19.5 8.25l-7.5 7.5l-7.5-7.5"/></svg>
+            @endif
+        </div>
     </a>
 </li>
--- resources/views/components/navigations/sidebar.blade.php
+++ resources/views/components/navigations/sidebar.blade.php
@@ -4,13 +4,26 @@
             <x-navigations.partials.nav-item
                 :url="$item['default_url']"
                 :title="$item['title']"
-                :isActive="request()->is($item['default_url'])">
+                :isActive="request()->is($item['default_url'])"
+                :hasSubLinks="isset($item['sub_links']) && count($item['sub_links']) > 0 ? true : false">
 
                 <x-slot name="icon">
                     {!! $item['icon'] !!}
                 </x-slot>
 
             </x-navigations.partials.nav-item>
+
+            @if (isset($item['sub_links']) && count($item['sub_links']))
+                <ul class="indent-5">
+                    @foreach ($item['sub_links'] as $link)
+                        <x-navigations.partials.nav-item
+                            :url="$link['default_url']"
+                            :title="$link['title']"
+                            :isActive="request()->is($link['default_url'])"
+                            :hasSubLinks="isset($link['sub_links']) && count($link['sub_links']) > 0 ? true : false" />
+                    @endforeach
+                </ul>
+            @endif
         @endforeach
     </ul>
 </aside>
